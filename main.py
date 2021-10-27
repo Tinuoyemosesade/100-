c61716268d358fb2d70bc6f9b5469367b158c406
moses <tinuoyemosesade@gmail.com>
2021-10-27 15:17:07 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,2 +1,8 @@
 #day1
-print("hello world!")
+#print ("hello world!")
+#print ("Day 1 - Python Print Function")
+#print ("the function is declared like this:")
+#print ('print("what to print")')
+##########################string manipulation
+print ("hello world\nhello world")
+
