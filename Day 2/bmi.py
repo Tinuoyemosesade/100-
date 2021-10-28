18a835dcdfdf54cee739b56320b30a7bf392606c
moses <tinuoyemosesade@gmail.com>
2021-10-28 15:03:59 +0200

    Create bmi.py

---
+++ Day 2/bmi.py
@@ -0,0 +1,8 @@
+# bmi
+height = float(input("please enter your height in m: "))
+weight = float(input("please enter your weight in kg: "))
+
+bmi = weight / (height ** 2)
+
+
+print( "your bmi is:" + str(int(bmi)))
