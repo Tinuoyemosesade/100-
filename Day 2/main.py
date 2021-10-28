06532b466a30b31852111e6f4f974a5b7de0f095
moses <tinuoyemosesade@gmail.com>
2021-10-28 11:28:29 +0200

    Create main.py

---
+++ Day 2/main.py
@@ -0,0 +1,16 @@
+# DataTYPES
+# float 3.14
+# integer 5988373
+# strings "hello world"
+# boolean  true or false
+
+# two_digit_number = input ("type a two digit number: ")
+# print(two_digit_number)
+# first_digit = int(two_digit_number[0])
+# second_digit = int(two_digit_number[1])
+
+# two_digit_number = first_digit + second_digit
+
+# print (two_digit_number)
+
+# 
