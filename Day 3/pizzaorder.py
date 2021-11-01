399892b1646ac8f3d8cdfcaba16f6d6c96e01c18
moses <tinuoyemosesade@gmail.com>
2021-11-01 16:02:49 +0100

    Create pizzaorder.py

---
+++ Day 3/pizzaorder.py
@@ -0,0 +1,26 @@
+print ("welcome to python pizza delivery")
+size = input (" what pizza size do you want? S, M, or L?")
+add_pepperoni = input ("do you want pepperoni? Y or N ")
+extra_cheese = input ("do you want extra cheese? Y or N?")
+
+bill = 0
+if size == "S":
+    bill += 15
+elif size == "M":
+    bill += 20
+else:
+    bill += 25
+
+if add_pepperoni == "Y":
+    if size =="S":
+        bill += 2
+    else:
+        bill += 3
+
+if extra_cheese == "Y":
+    bill += 1
+
+print(f"your total bill is ${bill}")
+
+
+
